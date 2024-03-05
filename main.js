210994ff3677290a4ac495b9e9a6e8e3d365aae1
daw2-barrio23 <barriomunoznicolas@fpllefia.com>
2024-03-05 19:35:47 +0100

    ejercicio 1.1 hecho

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("hola")
+
+//Ejercicio 1.1
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(usuarios => {
+        const tabla = document.getElementById('meteDatos')
+        usuarios.slice(0, 5).forEach(usuarios => {
+            const row = document.createElement('tr')
+            row.innerHTML = `
+            <td>${usuarios.id}</td>
+            <td>${usuarios.name.split(' ')[0]}</td>
+            <td>${usuarios.name.split(' ')[1]}</td>
+            <td>${usuarios.email}</td>`;
+
+            console.log(usuarios)
+            tabla.appendChild(row)
+            
+        });
+
+    })
+    .catch(error => console.error('Error'))
+//function usuarios(){}
